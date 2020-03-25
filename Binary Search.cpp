4c8864d9434c5cd2d4630340009b51b2f83f5b21
darrenleeleelee1 <darrenleeleelee1@gmail.com>
2020-03-25 22:19:10 +0800

    modify M

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -2,7 +2,7 @@ int L = 0;     //  left boundary
 int R = ans;   // right boundary
 
 while(L < R){
-	int M = (L + R + 1) / 2;  // floor
+	int M = L + (R + L) / 2;  // left + half distance
 	if(ok(M)) L = M;		  // ok() method is to find  whether the M can qualify the demand
 	else R = M - 1;
 }
