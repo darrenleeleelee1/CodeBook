7d322a127c814918c9dad42811881f988e4c9e65
darrenleeleelee1 <darrenleeleelee1@gmail.com>
2020-04-18 00:46:30 +0800

    Binary Search

---
+++ Search/Binary Search.cpp
@@ -0,0 +1,8 @@
+int L = 0;     //  left boundary
+int R = ans;   // right boundary
+
+while(L < R){
+	int M = L + (R - L) / 2;  // left + half distance
+	if(ok(M)) L = M;		  // ok() method is to find  whether the M can qualify the demand
+	else R = M - 1;
+}
