237c2913fff2f8ed9ce214dd38b9fa41cd9d4e2c
darrenleeleelee1 <darrenleeleelee1@gmail.com>
2020-05-12 20:01:36 +0800

    graph

---
+++ Graph/Disjoint Set.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int n = 6; // number of nodes 
+int parent[n+10];
+void init()
+{
+	for(int i = 0; i < n; i ++){
+		parent[i] = -1;
+	}
+}
+int find(int x)
+{
+	int xParent = x;
+	while(parent[xParent] >= 0){
+		xParent = parent[xParent];
+	}
+	parent[x] = xParent;
+	return xParent;
+}
+void unions(int x ,int y)
+{
+	int xParent = find(x);
+	int yParent = find(y);
+	if(xParent != yParent){
+		if(parent[xParent] > parent[yParent]){
+			parent[xParent] += parent[yParent];
+			parent[yParent] = xParent;
+		}
+		else{
+			parent[yParent] += parent[xParent];
+			parent[xParent] = yParent;	
+		}
+	}
+}
