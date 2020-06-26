2bf39a407890ab3b711d9a94dc8a4c8a0ba7e53b
darrenleeleelee1 <darrenleeleelee1@gmail.com>
2020-06-26 11:07:03 +0800

    upgrade Floyd Warshall

--- Graph/FloydWarshall.cpp
+++ Graph/FloydWarshall.cpp
@@ -1,5 +1,20 @@
 //dis[i][j] is the distance of node i to node j
-for (int k = 0; k < n; ++k)
-	for(int i = 0; i < n; ++i)
-		for(int j = 0; j < n; ++j)
-			dis[i][j] = min(dis[i][j],  dis[i][k] + dis[k][j]);
+int dis[n+5][n+5];
+void init()
+{
+	memset(dis, 0x3f, sizeof(dis));
+	for(int i = 0; i < n; i++) d[i][i] = 0;
+}
+void floyd(){
+	for (int k = 0; k < n; ++k)
+		for(int i = 0; i < n; ++i)
+			for(int j = 0; j < n; ++j)
+				dis[i][j] = dis[j][i] = min(dis[i][j],  dis[i][k] + dis[k][j]);	
+}
+int main(int argc, char const *argv[])
+{
+	//If we got n nodes, label from 0 to (n-1)
+	init();
+	//Set the dis
+	floyd();	
+}
