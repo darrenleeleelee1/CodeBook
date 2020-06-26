1ca72402fa39b9f1265da88374c3ebeabb69aa0d
darrenleeleelee1 <darrenleeleelee1@gmail.com>
2020-06-26 11:25:08 +0800

    BigInteger

---
+++ Basic/BigInteger.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        BigInteger n = input.nextBigInteger();
+        BigInteger m = input.nextBigInteger();
+        n.add(m); a.subtract(m); n.multiply(m); n.divide(m); n.mod(m);
+        n.pow(m.intValue()); n.gcd(m); n.negate(); n.abs();
+    }
+}
