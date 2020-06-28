55d4bcc2f80ec2f53033b4b94924a28b28a48024
darrenleeleelee1 <darrenleeleelee1@gmail.com>
2020-06-28 10:39:20 +0800

    upgrade Dijkstra

--- Graph/Dijkstra.cpp
+++ Graph/Dijkstra.cpp
@@ -1,29 +1,42 @@
-int maxn = ;
-int w[maxn][maxn], dis[maxn];
-vector<int> v[maxn];
-struct Node
+#define PII pair<int, int>
+#define maxn 50000+5
+
+int dis[maxn]; // 預設都是 INF
+vector<PII> adj[maxn]; // (連到的點,邊的距離)
+
+void dijk(int src) // dijk(起點)
+{ 
+  priority_queue<PII,vector<PII>,greater<PII>> q; // 放(點編號,距離)，每次會拿距離最小的點出來
+  q.push(make_pair(src, 0));
+    
+  	while (!q.empty()) 
+  	{
+    	auto top = q.top(); q.pop();
+  		printf("%d %d\n", top.first, top.second);
+    	if (dis[top.first] != 1e9) continue; // 如果之前就拜訪過就continue
+   
+    	dis[top.first] = top.second;
+    	for (auto i: adj[top.first]){
+        	if (dis[i.first] == 1e9) q.push(make_pair(i.first, i.second + top.second));     		
+    	}
+    }
+}
+
+void init(void)
 {
-	int node, weight;
-	Node(int _n, int _w){
-		node = _n;
-		weight = _w;
-	}	
-	bool operator<(Node const other)const{
-		return weight > other.weight;
-	}
-};
-void dijkstra(int src)
+    fill(dis, dis+maxn, 1e9);
+
+    for(int i = 0; i < maxn; i++){
+        adj[i].clear();
+    }
+}
+
+int main(int argc, char const *argv[])
 {
-	priority_queue<Node> pq;
-	pq.push(Node(src, 0));
-	while(!pq.empty())
-	{
-		auto top = pq.top();
-		pq.pop();
-		if(dis[top.node] != 1e9) continue;
-		for(auto i : v[top.node]){
-			pq.push(Node(i, top.weight + w[top.node][i]));
-		}
-		dis[top.node] = top.weight;
-	}
+	init();
+	// make adjcnet list
+	int sp = 0; // start point
+	dijk(sp);
+	// dis[x] store the distance from sp to x
+	return 0;
 }
